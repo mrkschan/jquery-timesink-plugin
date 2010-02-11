8b53d0abd933170e2f4cf808321615ecefffa82b
mrkschan <mr.kschan@gmail.com>
2010-02-11 16:49:58 +0800

    show-hide will now dispatch events

--- jquery.timesink.js
+++ jquery.timesink.js
@@ -1,13 +1,14 @@
 jQuery.fn.timeSink = function(list, range, mapper)
 {
 	var ts = this;
+	this.sieve = null;
 	list = jQuery(list);
 
 	if ( list.length ) {
 		var rows = list.children('li');
 		var cache = (mapper)
 				? rows.map(mapper)
-				: rows.map(function(){
+				: rows.map(function() {
 					return new Date(this.innerHTML);
 				});
 
@@ -23,13 +24,21 @@ jQuery.fn.timeSink = function(list, range, mapper)
 		$(el).click(function ()
 		{
 			ts.sieve = sieve;
-			cache.each(function(i)
+			cache.each(function(i, c)
 			{
 				if (null == sieve) {
 					rows.show(); // show all
+					rows.each(function(_i, el) {
+						$(el).trigger('timesink-show', $(el));
+					});
 				} else {
-					if (cache[i] >= sieve) $(rows[i]).show();
-					else                   $(rows[i]).hide();
+					if (c >= sieve) {
+						$(rows[i]).show();
+						$(rows[i]).trigger('timesink-show', $(rows[i]));
+					} else {
+						$(rows[i]).hide();
+						$(rows[i]).trigger('timesink-hide', $(rows[i]));
+					}
 				}
 			});
 		});
